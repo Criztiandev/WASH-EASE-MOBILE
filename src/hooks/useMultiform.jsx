0f743016ea0e77bc84ee93c7daae957d124e7490
criztiandev <criztiandev@gmail.com>
2024-04-20 03:02:40 +0800

    Added Washing machine

---
+++ src/hooks/useMultiform.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const useMultiform = () => {
+    //TODO: Create a multi form
+  return (
+ >
+  );
+};
+
+export default useMultiform;
