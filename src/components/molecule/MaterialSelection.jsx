e74ba1a740495af16ae4d096cba6ab36fc7fc604
criztiandev <criztiandev@gmail.com>
2024-04-30 02:10:25 +0800

    Fixed Material selection

--- src/components/molecule/MaterialSelection.jsx
+++ src/components/molecule/MaterialSelection.jsx
@@ -14,6 +14,8 @@ import { ToggleButton } from "react-native-paper";
 import { useController } from "react-hook-form";
 import Checkbox from "expo-checkbox";
 import { atom, useAtom, useAtomValue, useSetAtom } from "jotai";
+import { FlashList } from "@shopify/flash-list";
+import Button from "../atoms/Button";
 
 const MOCKDATA = [
   { id: 0, title: "Test" },
@@ -25,59 +27,70 @@ const MOCKDATA = [
   { id: 6, title: "Test" },
 ];
 
-const MaterialSelectedItems = atom([]);
 const MaterialSelection = ({ form, name }) => {
-  const selectedItems = useAtomValue(MaterialSelectedItems);
-  const { field } = useController({
-    control: form.control,
-    name,
-  });
+  const { field } = useController({ control: form.control, name });
+  const [selectedItems, setSelectedItems] = useState([]);
 
   useEffect(() => {
-    if (selectedItems) {
-      form.setValue("basic-material", selectedItems);
-    }
-  }, [selectedItems]);
+    form.setValue(name, selectedItems);
+  }, [selectedItems, form, name]);
 
   const renderItem = useCallback(
-    ({ item }) => <ServiceItem field={field} key={item.id} value={item.id} />,
+    ({ item }) => (
+      <MaterialItem
+        field={field}
+        key={item.id}
+        value={item.id}
+        onSelect={setSelectedItems}
+        isActive={selectedItems.includes(item.id)}
+      />
+    ),
     []
   );
   const keyExtractor = useCallback((item) => item.id.toString(), []);
 
   return (
     <>
-      <View>
-        <View className="items-center justify-center my-4">
-          <Text className="text-[24px] font-semibold">Select Material</Text>
-        </View>
+      <View className="flex-1 border w-full mb-4">
+        <Text className="text-[24px] font-semibold text-center my-4">
+          Select Material
+        </Text>
 
-        <FlatList
+        <FlashList
           data={MOCKDATA}
           renderItem={renderItem}
           keyExtractor={keyExtractor}
-          className="mb-[220px] px-2"
+          estimatedItemSize={200}
         />
       </View>
     </>
   );
 };
 
-const ServiceItem = memo(({ value }) => {
-  const setSelectedItems = useSetAtom(MaterialSelectedItems);
-  const [checked, setChecked] = useState(false);
-  const [quantity, setQuantity] = useState(1);
+const MaterialItem = memo(({ value, onSelect, isActive = false }) => {
+  const [checked, setChecked] = useState(isActive);
+  const [quantity, setQuantity] = useState(0);
 
   const handleIncrement = () => setQuantity((prev) => Math.min(prev + 1, 99));
   const handleDecrement = () => setQuantity((prev) => Math.max(prev - 1, 0));
 
   const handleSelect = useCallback(() => {
-    setChecked((prevChecked) => !prevChecked);
+    setChecked((prevState) => {
+      const newState = !prevState;
+
+      if (newState) {
+        handleIncrement();
+      } else {
+        handleDecrement();
+      }
+
+      return newState;
+    });
   }, []);
 
   useEffect(() => {
     if (checked) {
-      setSelectedItems((prev) => {
+      onSelect((prev) => {
         const index = prev.findIndex((item) => item.id === value);
         if (index > -1) {
           return prev.map((item) =>
@@ -88,49 +101,56 @@ const ServiceItem = memo(({ value }) => {
         }
       });
     } else {
-      setSelectedItems((prev) => prev.filter((item) => item.id !== value));
+      onSelect((prev) => prev.filter((item) => item.id !== value));
     }
   }, [checked, quantity]);
 
   return (
     <TouchableOpacity
       className={cn(
-        `p-4 bg-white mb-2 rounded-md border border-gray-200 ${
-          checked ? "border-2 border-blue-700" : "border-none"
-        }`
+        `${
+          checked && "bg-blue-300/50 border-2 border-blue-400"
+        } rounded-[5px] mb-2`
       )}
       onPress={handleSelect}>
-      <View className="flex-row space-x-4 justify-between items-center ">
-        <View className="flex-row space-x-4">
-          <View className="w-[64px] h-[64px] flex border rounded-md"></View>
-          <View>
-            <Text className="text-[18px] font-bold">Regular Wash</Text>
-            <View className="flex-row space-x-2 items-center">
-              <Text className="text-[24px] font-bold">P 670</Text>
-              {checked && (
-                <Text className="text-[18px] font-bold text-gray-400">
-                  X {quantity}
-                </Text>
-              )}
+      <View className=" max-h-[150px] py-4 px-2 flex-row ">
+        <View className="justify-between flex-row  flex-1">
+          {/* Details */}
+          <View
+            className="flex-row space-x-3 justify-between items-center"
+            style={{ flexShrink: 1 }}>
+            {quantity > 0 && (
+              <Badge
+                className={
+                  "absolute -top-3 left-1 text-[14px] w-[24px] h-[24px] rounded-full"
+                }>
+                {quantity}
+              </Badge>
+            )}
+            <View className="w-[64px] h-[64px] border rounded-[5px]"></View>
+            <View>
+              <Text className="text-[18px] font-bold">Regular Wash</Text>
+              <Text className="text-[18px]">950</Text>
             </View>
           </View>
-        </View>
-        <View className="justify-center">
+
+          {/* Actions */}
           {checked && (
-            <>
+            <View className="flex-row">
               <IconButton
-                icon={"plus"}
+                icon="plus"
                 size={24}
                 onPress={handleIncrement}
-                className="bg-gray-300"
+                className=" bg-gray-400/50"
               />
+
               <IconButton
-                icon={"minus"}
+                icon="minus"
                 size={24}
                 onPress={handleDecrement}
-                className="bg-gray-300"
+                className=" bg-gray-400/50"
               />
-            </>
+            </View>
           )}
         </View>
       </View>
